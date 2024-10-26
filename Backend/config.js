ad2501289aaf56c2fa89a304b64dccb1ad00b155
Apple <apple@LAKSHYAmac.local>
2024-10-26 22:25:16 +0530

    initial commit

---
+++ Backend/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://dawnvi0410be21:6BbKweNurOLYDrGZ@cluster0.jekpn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    port: process.env.PORT || 5000,
+  };
